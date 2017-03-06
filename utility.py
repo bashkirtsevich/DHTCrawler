dd89c57fad44dfc35406437e79483ca25e852eb5
bashkirtsevich <bashkirtsevich@gmail.com>
2017-03-06 16:02:56 +0500

    Rename unused parameter

--- utility.py
+++ utility.py
@@ -10,7 +10,7 @@ from random import randint
 
 
 def generate_id(length):
-    return reduce(lambda r, i: r + chr(randint(0, 255)), range(length), "")
+    return reduce(lambda r, _: r + chr(randint(0, 255)), range(length), "")
 
 
 def generate_node_id():
